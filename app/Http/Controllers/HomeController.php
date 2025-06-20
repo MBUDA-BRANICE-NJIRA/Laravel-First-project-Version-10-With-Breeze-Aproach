1e6a4a5229c181bd5c377679272c45ad0cead263
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-06-20 12:06:30 +0300

    worked on home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers;
 
-
-
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('Home');
+        return view('Home'); // This matches your resources/views/Home.blade.php
     }
 }
